7529c890a6d3aa6b7ab2353d18b770bca16385e4
XiaoGuang Zeng <xgzeng@gmail.com>
2016-08-04 10:25:11 +0800

    update cmake files

--- cmake/FindCppFormat.cmake
+++ cmake/FindCppFormat.cmake
@@ -3,7 +3,7 @@ find_library(CPPFORMAT_LIBRARY cppformat)
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(CppFormat
+find_package_handle_standard_args(CPPFORMAT
     REQUIRED_VARS CPPFORMAT_INCLUDE_DIR CPPFORMAT_LIBRARY)
 
 #if(NOT CppFormat_FOUND)
--- cmake/UseNlohmannJson.cmake
+++ cmake/UseNlohmannJson.cmake
@@ -1,5 +1,5 @@
 
-if(NOT NlohmannJson_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/third_party/nlohmann_json")
+if(NOT NlohmannJson_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/third_party/nlohmann_json/CMakeLists.txt")
   add_subdirectory("third_party/nlohmann_json")
   set(NLOHMANN_JSON_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/nlohmann_json/src")
   set(NlohmannJson_FOUND TRUE)
