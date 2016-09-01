a2699008ca634a0b413e7b591535fdb37fbc3176
XiaoGuang Zeng <xgzeng@gmail.com>
2016-09-01 14:13:23 +0800

    use conan to manage dependency

--- cmake/UseNlohmannJson.cmake
+++
@@ -1,8 +0,0 @@
-
-if(NOT NlohmannJson_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/third_party/nlohmann_json/CMakeLists.txt")
-  add_subdirectory("third_party/nlohmann_json")
-  set(NLOHMANN_JSON_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/nlohmann_json/src")
-  set(NlohmannJson_FOUND TRUE)
-endif()
-
-
