2abd4fdd330feacd15e4506792e1a5ef98f7daff
XiaoGuang Zeng <xgzeng@gmail.com>
2016-09-26 13:15:07 +0800

    Use localtime_r to replace unsafe localtime

--- cpplog/console_sink_unix.h
+++ cpplog/console_sink_unix.h
@@ -3,7 +3,8 @@
 class ConsoleSinkUnix : public LogSink {
 public:
   void SubmitRecord(LogRecord& r) override {
-    struct tm * tm_time = localtime(&r.timestamp().tv_sec);
+    struct tm tm_local {};
+    localtime_r(&r.timestamp().tv_sec, &tm_local);
 
     const char* base_filename = strrchr(r.file_name(), '/');
     base_filename = base_filename ? base_filename + 1 : r.file_name();
@@ -13,12 +14,12 @@ public:
     std::cout << "\033[0;3" << (int)SeverityColor(r.level()) << 'm'
         << LevelLetter(r.level())
         << setfill('0')
-        << setw(2) << 1 + tm_time->tm_mon
-        << setw(2) << tm_time->tm_mday
+        << setw(2) << 1 + tm_local.tm_mon
+        << setw(2) << tm_local.tm_mday
         << ' '
-        << setw(2) << tm_time->tm_hour << ':'
-        << setw(2) << tm_time->tm_min << ':'
-        << setw(2) << tm_time->tm_sec << '.'
+        << setw(2) << tm_local.tm_hour << ':'
+        << setw(2) << tm_local.tm_min << ':'
+        << setw(2) << tm_local.tm_sec << '.'
         << setw(4) << (r.timestamp().tv_nsec / 1000000)
         << ' '
         << base_filename << ':' << r.line() << ':' << r.function_name() << "] "
--- cpplog/console_sink_windows.h
+++ cpplog/console_sink_windows.h
@@ -5,9 +5,8 @@ class ConsoleSinkWindows : public LogSink {
   HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 public:
   void SubmitRecord(LogRecord& r) override {
-    struct tm tm_time_ {};
-    localtime_s(&tm_time_, &r.timestamp().tv_sec);
-    struct tm * tm_time = &tm_time_;
+    struct tm tm_local {};
+    localtime_s(&tm_local, &r.timestamp().tv_sec);
 
     const char* base_filename = strrchr(r.file_name(), '/');
     if (!base_filename) base_filename = strrchr(r.file_name(), '\\');
@@ -34,12 +33,12 @@ public:
     using std::setfill;
     std::cout << LevelLetter(r.level())
       << setfill('0')
-      << setw(2) << 1 + tm_time->tm_mon
-      << setw(2) << tm_time->tm_mday
+      << setw(2) << 1 + tm_local.tm_mon
+      << setw(2) << tm_local.tm_mday
       << ' '
-      << setw(2) << tm_time->tm_hour << ':'
-      << setw(2) << tm_time->tm_min << ':'
-      << setw(2) << tm_time->tm_sec << '.'
+      << setw(2) << tm_local.tm_hour << ':'
+      << setw(2) << tm_local.tm_min << ':'
+      << setw(2) << tm_local.tm_sec << '.'
       << setw(4) << (r.timestamp().tv_nsec / 1000000)
       << ' '
       << base_filename << ':' << r.line() << ':' << r.function_name() << "] "
