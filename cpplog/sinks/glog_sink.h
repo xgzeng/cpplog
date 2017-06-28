e90c16f8f25ec5b7889a2d87190a2c126332bf19
XiaoGuang Zeng <xgzeng@gmail.com>
2017-06-28 15:59:22 +0800

    update GlogSink to compatible with new version

--- cpplog/sinks/glog_sink.h
+++ cpplog/sinks/glog_sink.h
@@ -10,16 +10,18 @@ public:
   void SubmitRecord(const LogRecord& r) override {
     google::LogSeverity severity = google::GLOG_INFO;
     switch (r.level()) {
-    case LogLevel::trace:
-    case LogLevel::debug:
-    case LogLevel::info:    severity = google::GLOG_INFO; break;
-    case LogLevel::warning: severity = google::GLOG_WARNING; break;
-    case LogLevel::error:   severity = google::GLOG_ERROR; break;
-    case LogLevel::fatal:   severity = google::GLOG_FATAL; break;
+    case LogLevel::Trace:
+    case LogLevel::Debug:
+    case LogLevel::Information:    severity = google::GLOG_INFO; break;
+    case LogLevel::Warning: severity = google::GLOG_WARNING; break;
+    case LogLevel::Error:   severity = google::GLOG_ERROR; break;
+    case LogLevel::Fatal:   severity = google::GLOG_FATAL; break;
     default:
       break;
     }
-    google::LogMessage(r.file_name(), r.line(), severity, 0, &google::LogMessage::SendToLog).stream() << r.message();
+    auto& src_file = r.src_file_info();
+    google::LogMessage(src_file.file_name(), src_file.line(), severity, 0,
+                       &google::LogMessage::SendToLog).stream() << r.message();
   }
 };
 
