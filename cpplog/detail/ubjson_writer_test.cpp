5f235936b08af16e5e3fa2ccc14b7428b21b2d53
XiaoGuang Zeng <xgzeng@gmail.com>
2018-04-02 09:35:05 +0800

    add test for is_safe_integer_cast template

--- cpplog/detail/ubjson_writer_test.cpp
+++ cpplog/detail/ubjson_writer_test.cpp
@@ -225,3 +225,76 @@ TEST_CASE("UBJsonWriter Result can be parsed by nlohmann::json") {
 
   REQUIRE(json == expected);
 }
+
+TEST_CASE("test is_safe_integer_cast<> template") {
+  using namespace cpplog::detail;
+  
+  // unsigned to unsigned
+  static_assert(is_safe_integer_cast<uint8_t, uint8_t>::value, "");
+  static_assert(is_safe_integer_cast<uint8_t, uint16_t>::value, "");
+  static_assert(is_safe_integer_cast<uint8_t, uint32_t>::value, "");
+  static_assert(is_safe_integer_cast<uint8_t, uint64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<uint16_t, uint8_t>::value, "");
+  static_assert( is_safe_integer_cast<uint16_t, uint16_t>::value, "");
+  static_assert( is_safe_integer_cast<uint16_t, uint32_t>::value, "");
+  static_assert( is_safe_integer_cast<uint16_t, uint64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<uint32_t, uint8_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint32_t, uint16_t>::value, "");
+  static_assert( is_safe_integer_cast<uint32_t, uint32_t>::value, "");
+  static_assert( is_safe_integer_cast<uint64_t, uint64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<uint64_t, uint8_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint64_t, uint16_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint64_t, uint32_t>::value, "");
+  static_assert( is_safe_integer_cast<uint64_t, uint64_t>::value, "");
+
+  // signed to signed
+  static_assert(is_safe_integer_cast<int8_t, int8_t>::value, "");
+  static_assert(is_safe_integer_cast<int8_t, int16_t>::value, "");
+  static_assert(is_safe_integer_cast<int8_t, int32_t>::value, "");
+  static_assert(is_safe_integer_cast<int8_t, int64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<int16_t, int8_t>::value, "");
+  static_assert( is_safe_integer_cast<int16_t, int16_t>::value, "");
+  static_assert( is_safe_integer_cast<int16_t, int32_t>::value, "");
+  static_assert( is_safe_integer_cast<int16_t, int64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<int32_t, int8_t>::value, "");
+  static_assert(!is_safe_integer_cast<int32_t, int16_t>::value, "");
+  static_assert( is_safe_integer_cast<int32_t, int32_t>::value, "");
+  static_assert( is_safe_integer_cast<int32_t, int64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<int64_t, int8_t>::value, "");
+  static_assert(!is_safe_integer_cast<int64_t, int16_t>::value, "");
+  static_assert(!is_safe_integer_cast<int64_t, int32_t>::value, "");
+  static_assert( is_safe_integer_cast<int64_t, int64_t>::value, "");
+
+
+  // unsigned to signed
+  // unsigned type is safe to cast to signed type, if type range is samller
+  static_assert(!is_safe_integer_cast<uint8_t, int8_t>::value, "");
+  static_assert( is_safe_integer_cast<uint8_t, int16_t>::value, "");
+  static_assert( is_safe_integer_cast<uint8_t, int32_t>::value, "");
+  static_assert( is_safe_integer_cast<uint8_t, int64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<uint16_t, int8_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint16_t, int16_t>::value, "");
+  static_assert( is_safe_integer_cast<uint16_t, int32_t>::value, "");
+  static_assert( is_safe_integer_cast<uint16_t, int64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<uint32_t, int8_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint32_t, int16_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint32_t, int32_t>::value, "");
+  static_assert( is_safe_integer_cast<uint32_t, int64_t>::value, "");
+
+  static_assert(!is_safe_integer_cast<uint64_t, int8_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint64_t, int16_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint64_t, int32_t>::value, "");
+  static_assert(!is_safe_integer_cast<uint64_t, int64_t>::value, "");
+
+  // signed to unsigned
+  // signed type is not safe to cast to signed type
+  static_assert(!is_safe_integer_cast<int8_t, uint64_t>::value, "");
+}
